ce11f89e0331703544ff5f9367afd5745c133b7c
duressa-feyissa <duresafeyisa2022@gmail.com>
2024-03-31 02:30:42 +0300

    :sparkles: Add role DTO and Validation t at application layer

---
+++ backend/Application/DTO/Common/Role/DTO/RoleResponseDTO.cs
@@ -0,0 +1,13 @@
+namespace Application.DTO.Common.Role.DTO
+{
+    public class RoleResponseDTO
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public required string Code { get; set; }
+
+    }
+}
