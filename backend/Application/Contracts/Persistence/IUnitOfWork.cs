abeb95921a2b3b6ea0b4156bffab07df8f641c27
duressa-feyissa <duresafeyisa2022@gmail.com>
2024-03-31 03:51:55 +0300

    :sparkles: Add User contract at application layer

--- backend/Application/Contracts/Persistence/IUnitOfWork.cs
+++ backend/Application/Contracts/Persistence/IUnitOfWork.cs
@@ -1,23 +1,26 @@
+using System.Threading.Tasks;
 using Application.Contracts.Persistence.Repositories.Common;
 using Application.Contracts.Persistence.Repositories.Product;
+using Application.Contracts.Persistence.Repositories.User;
 
 namespace Application.Contracts.Persistance.Repositories
 {
-	public interface IUnitOfWork
-	{
-		IColorRepository ColorRepository { get; }
-		IProductRepository ProductRepository { get; }
-		ISizeRepository SizeRepository { get; }
-		IBrandRepository BrandRepository { get; }
-		IMaterialRepository MaterialRepository { get; }
-		IProductColorRepository ProductColorRepository { get; }
-		IProductSizeRepository ProductSizeRepository { get; }
-		IProductMaterialRepository ProductMaterialRepository { get; }
-		IProductImageRepository ProductImageRepository { get; }
-		ILocationRepository LocationRepository { get; }
-		ICategoryRepository CategoryRepository { get; }
-		IRoleRepository RoleRepository { get; }
-		IProductCategoryRepository ProductCategoryRepository { get; }
-		Task<int> Save();
-	}
+    public interface IUnitOfWork
+    {
+        IColorRepository ColorRepository { get; }
+        IProductRepository ProductRepository { get; }
+        ISizeRepository SizeRepository { get; }
+        IBrandRepository BrandRepository { get; }
+        IMaterialRepository MaterialRepository { get; }
+        IProductColorRepository ProductColorRepository { get; }
+        IProductSizeRepository ProductSizeRepository { get; }
+        IProductMaterialRepository ProductMaterialRepository { get; }
+        IProductImageRepository ProductImageRepository { get; }
+        ILocationRepository LocationRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+        IRoleRepository RoleRepository { get; }
+        IProductCategoryRepository ProductCategoryRepository { get; }
+        IUserRepository UserRepository { get; }
+        Task<int> Save();
+    }
 }
