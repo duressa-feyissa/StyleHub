9dd72077139b17ebb85095eb3b42361994791b5a
duressa-feyissa <duresa.feyisa.gose@gmail.com>
2024-08-25 07:50:57 +0300

    :bug: modify user type response

--- backend/Application/DTO/User/UserDTO/DTO/UserResponseDTO.cs
+++ backend/Application/DTO/User/UserDTO/DTO/UserResponseDTO.cs
@@ -9,13 +9,15 @@ namespace backend.Application.DTO.User.UserDTO.DTO
 		public string? LastName { get; set; }
 		public string? PhoneNumber { get; set; }
 		public string? Email { get; set; }
-		public string? Latitude { get; set; }
-		public string? Longitude { get; set; }
+		public double? Latitude { get; set; }
+		public double? Longitude { get; set; }
 		public string? ProfilePicture { get; set; }
 		public string? Street { get; set; }
 		public string? SubLocality { get; set; }
 		public string? SubAdministrativeArea { get; set; }
 		public string? PostalCode { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public string? Gender { get; set; }
 		public required RoleResponseDTO Role { get; set; }
 	}
 }
